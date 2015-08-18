37354c0fd360f77b2d6b1d168536da9181791e8f
Nikolaibibo <nbockholt@fork.de>
2015-08-18 18:39:00 +0200

    set size to 400x300

--- ffmpeghelper.js
+++ ffmpeghelper.js
@@ -20,11 +20,11 @@ var target_file_h264 = "/home/pi/nodejs/gifittome_terminal/public/videos/video.h
 var target_file_qr = "/home/pi/nodejs/gifittome_terminal/public/images/qr.png";
 
 // shell string for shell.js
-var shell_string_stillimage = "raspistill -o " + target_file_still + " -w 600 -h 400 -t 500";
+var shell_string_stillimage = "raspistill -o " + target_file_still + " -w 400 -h 300 -t 500";
 var shell_string_delete = "rm -r -f /home/pi/nodejs/gifittome_terminal/public/videos/*";
-var shell_string_create_video = "raspivid -o " + target_file_h264 + " -fps 25 -w 600 -h 400 -t 5000";
+var shell_string_create_video = "raspivid -o " + target_file_h264 + " -fps 25 -w 400 -h 300 -t 5000";
 var shell_string_create_watermark = "ffmpeg -i " + target_file_h264 + " -i " + target_file_watermark + " -filter_complex 'overlay=0:0' " + target_file_mp4;
-var shell_string_ffmpeg_palette = "ffmpeg -i " + target_file_h264 + " -vf 'fps=5,scale=600:-1:flags=lanczos,palettegen' -y " + target_file_palette;
+var shell_string_ffmpeg_palette = "ffmpeg -i " + target_file_h264 + " -vf 'fps=5,scale=400:-1:flags=lanczos,palettegen' -y " + target_file_palette;
 
 
 function FfmpegHelper () {
@@ -74,7 +74,7 @@ FfmpegHelper.prototype.createGIF = function () {
     var datestring = d.getDate() + "_" + d.getMonth() + "_" + d.getFullYear() + "_" + d.getHours() + "-" + d.getMinutes() + "_video.gif";
     target_file_gif = datestring;
 
-    var shell_string_ffmpeg_gif = "ffmpeg -i " + target_file_mp4 + " -i " + target_file_palette + " -lavfi 'fps=15,scale=600:-1:flags=lanczos [x]; [x][1:v] paletteuse' -y " + target_folder_gif_path + target_file_gif;
+    var shell_string_ffmpeg_gif = "ffmpeg -i " + target_file_mp4 + " -i " + target_file_palette + " -lavfi 'fps=15,scale=400:-1:flags=lanczos [x]; [x][1:v] paletteuse' -y " + target_folder_gif_path + target_file_gif;
     console.log("shell_string_ffmpeg_gif::::: " + shell_string_ffmpeg_gif);
 
     shell.exec(shell_string_ffmpeg_gif, function(code, output) {
